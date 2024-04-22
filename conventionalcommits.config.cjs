ceb9c3b790b22eb847169108abe253e9aa2b3c44
Juri Leino <github@line-o.de>
2024-04-22 18:33:43 +0200

    ci: minor changes to release rules

--- conventionalcommits.config.cjs
+++ conventionalcommits.config.cjs
@@ -1,3 +1,7 @@
+// Change default header pattern matcher to match headers with no colon.
+// This will make the first __word__ in free-form commit message headers the __type__.
+// With this non-empty type we can do a negated match (the type being anything but
+// one of the other keywords with associated release rules).
 module.exports = { 
     headerPattern: /^(\w*)(?:\(([\w\$\.\-\* ]*)\))?\:? (.*)$/
 }
